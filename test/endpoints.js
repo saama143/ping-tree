633599c03558877c72b75cfcf87ed1ebf79ab880
unknown <usamaabdullah12@gmail.com>
2020-10-09 10:11:02 +0500

    completed

--- test/endpoints.js
+++ test/endpoints.js
@@ -15,3 +15,26 @@ test.serial.cb('healthcheck', function (t) {
     t.end()
   })
 })
+
+test.serial.cb('Get All Targets', function (t) {
+  var url = '/api/targets'
+  servertest(server(), url, { encoding: 'json' }, function (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.body.status, 'OK', 'status is ok')
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.end()
+  })
+})
+
+test.serial.cb('Get Target By Id', function (t) {
+  var url = '/api/target/1'
+  servertest(server(), url, { encoding: 'json' }, function (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.body.status, 'OK', 'status is ok')
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.end()
+  })
+})
+
